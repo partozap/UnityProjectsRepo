e14db2f82a4b3fcff7c5de72110602ba460a11c5
partozap <ppartoza@gmail.com>
2020-04-30 12:40:02 -0700

    ML Agents Addition

---
+++ Penguin Agents/Assets/Penguin/Scripts/PenguinAgent.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+using MLAgents.Sensors;
+
+public class PenguinAgent : Agent
+{
+    //How fast the agent moves forward
+    public float moveSpeed = 5f;
+
+    //how fast the agent turns
+    public float turnspeed = 180f;
+
+    //Prefab of the heart that appears when the baby is fed
+    public GameObject heartPrefab;
+
+    //Prefab of the regurgitated fish that appears when the baby is fed
+    public GameObject regurgitatedFishPrefab;
+
+    //private variables that track everything
+    private PenguinArea penguinArea;
+    new private Rigidbody rigidbody;
+    private GameObject baby;
+    private bool isFull; //If true, penguin has a full stomach
+    private float feedRadius = 0f;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        penguinArea = GetComponentInParent<PenguinArea>();
+        baby = penguinArea.penguinBaby;
+        rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    //Performs actions based on a vector of numbers
+    public override void AgentAction(float[] vectorAction)
+    {
+        //convert the first action to forward movement
+        float forwardAmount = vectorAction[0];
+
+        //Convert the second action to turning left or right
+        float turnAmount = 0f;
+        if (vectorAction[1] == 1f)
+        {
+            turnAmount = -1f;
+        }
+        else if (vectorAction[1] == 2f)
+        {
+            turnAmount = 1f;
+        }
+
+        //Apply movement
+        rigidbody.MovePosition(transform.position + transform.forward * forwardAmount * moveSpeed * Time.fixedDeltaTime);
+        transform.Rotate(transform.up * turnAmount * turnspeed * Time.fixedDeltaTime);
+
+        //Apply a tiny negative reward every step to encourage action
+        if (maxStep > 0) AddReward(-1 / maxStep);
+    
+    }
+
+    //Read inputs from the keyboard and convert them to a list of actions
+    //Only called when the player wants to control the agent and as set behavior type to "Heuristic Only" in the Behavior Parameters inspector
+    public override float[] Heuristic()
+    {
+        float forwardAction = 0f;
+        float turnAction = 0f;
+        if (Input.GetKey(KeyCode.W))
+        {
+            forwardAction = 1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            turnAction = 1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            turnAction = 2f;
+        }
+
+        return new float[] { forwardAction, turnAction };
+    }
+
+    //Reset the agent and area
+    public override void AgentReset()
+    {
+        isFull = false;
+        penguinArea.ResetArea();
+        feedRadius = Academy.Instance.FloatProperties.GetPropertyWithDefault("feed_radius", 0f);
+    }
+
+    /// The code here is the corrected implementation off of the tutorial. The original tutorial had a patched out 
+    /// syntax for everything. This is the corrected version. If you run into an error, this is where it may stem from
+
+    public override void CollectObservations(MLAgents.Sensors.VectorSensor sensor)
+    {
+        base.CollectObservations(sensor);
+        
+        //Whether the penguin has eaten a fish (1 float = 1 value)
+        sensor.AddObservation(isFull);
+
+        //Distance to the baby (1 float = 1 value)
+        sensor.AddObservation(Vector3.Distance(baby.transform.position, transform.position));
+
+        //Direction to baby (1 Vector3 = 3 values)
+        sensor.AddObservation((baby.transform.position - transform.position).normalized);
+
+        //Direction penguin is facing (1 Vector3 = 3 values)
+        sensor.AddObservation(transform.forward);
+    }
+
+    private void FixedUpdate()
+    {
+        /*Request a decision every 5 steps. RequestDecision() automatically calls RequestAction(),
+         * but for the steps in between, we need to call it explicitly to take action using 
+         * the results of the previous decision */
+
+        //Use "StepCount" rather than "GetStepCount()"
+        if (StepCount %5 == 0)
+        {
+            RequestDecision();
+        }
+
+        else
+        {
+            RequestAction();
+        }
+
+        // Test if the agent is close enough to feed the baby
+        if (Vector3.Distance(transform.position, baby.transform.position) < feedRadius)
+        {
+            //Close enough, try to feed the baby
+            RegurgitateFish();
+        }
+
+    }
+
+    //When the agent collides with something, take action
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("fish"))
+        {
+            //Try to eat the fish
+            EatFish(collision.gameObject);
+        }
+        else if (collision.transform.CompareTag("baby"))
+        {
+            //try to feed the baby
+            RegurgitateFish();
+        }
+    }
+    //Check if agent is full, if not, eat the fish and get a reward
+    private void EatFish(GameObject fishObject)
+    {
+        if (isFull) return; //Can't eat on a full stomach
+        isFull = true;
+
+        penguinArea.RemoveSpecificFish(fishObject);
+
+        AddReward(1f);
+    }
+
+    //Check if agent is full, if yes, feed the baby
+
+    private void RegurgitateFish()
+    {
+        if (!isFull) return; //Nothing to regurgitate
+        isFull = false;
+
+        //Spawn regurgitated fish
+        GameObject regurgitatedFish = Instantiate<GameObject>(regurgitatedFishPrefab);
+        regurgitatedFish.transform.parent = transform.parent;
+        regurgitatedFish.transform.position = baby.transform.position;
+        Destroy(regurgitatedFish, 4f);
+
+        //Spawn a heart
+        GameObject heart = Instantiate<GameObject>(heartPrefab);
+        heart.transform.parent = transform.parent;
+        heart.transform.position = baby.transform.position + Vector3.up;
+        Destroy(heart, 4f);
+
+        AddReward(1f);
+
+        if (penguinArea.FishRemaining <= 0)
+        {
+            Done();
+        }
+    }
+    
+}
---
+++ Penguin Agents/Assets/Penguin/Scripts/PenguinArea.cs
@@ -0,0 +1,133 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+using TMPro;
+
+public class PenguinArea : MonoBehaviour
+{
+    //The agent inside the area
+    public PenguinAgent penguinAgent;
+
+    //The baby penguin inside the are
+    public GameObject penguinBaby;
+
+    //The TextMeshPro text that shows the cumulative reward of the agent
+    public TextMeshPro cumulativeRewardText;
+
+    //Prefab of a live fish
+    public Fish fishPrefab;
+
+    private List<GameObject> fishList;
+
+    //This resets the area, including the fish and penguin placement
+    public void ResetArea()
+    {
+        RemoveAllFish();
+        PlacePenguin();
+        PlaceBaby();
+        SpawnFish(4, Academy.Instance.FloatProperties.GetPropertyWithDefault("fish_speed", 05f));
+
+    }
+
+    //Removes a specific fish from the area when it is eaten
+    public void RemoveSpecificFish( GameObject fishObject)
+    {
+        fishList.Remove(fishObject);
+        Destroy(fishObject);
+    
+    }
+
+    //The number of fish remaining
+    public int FishRemaining
+    {
+        get { return fishList.Count; }
+    }
+
+    //Choose a random position on the X-Z plane within a partial donut shape
+
+    public static Vector3 ChooseRandomPosition(Vector3 center, float minAngle, float maxAngle, float minRadius, float maxRadius)
+    {
+        float radius = minRadius;
+        float angle = minAngle;
+
+        if (maxRadius > minRadius)
+        {
+            //Pick a random radius
+            radius = UnityEngine.Random.Range(minAngle, maxAngle);
+        }
+
+        if (maxAngle > minAngle)
+        {
+            //Pick a random angle
+            angle = UnityEngine.Random.Range(minAngle, maxAngle);
+        }
+
+        //center position + forward vector rotated around the y axis by "angle" degrees, multiplies by "radius"
+        return center + Quaternion.Euler(0f, angle, 0f) * Vector3.forward * radius;
+    }
+    //Removes all fish from the area
+    private void RemoveAllFish()
+    {
+        if (fishList != null)
+        {
+            for (int i = 0; i < fishList.Count; i++)
+            {
+                if (fishList[i] != null)
+                {
+                    Destroy(fishList[i]);
+                }
+            }
+        }
+        fishList = new List<GameObject>();
+    }
+    //Places the penguin in the area
+    private void PlacePenguin()
+    {
+        Rigidbody rigidbody = penguinAgent.GetComponent<Rigidbody>();
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+        penguinAgent.transform.position = ChooseRandomPosition(transform.position, 0f, 360f, 0f, 1f) + Vector3.up * .5f;
+        penguinAgent.transform.rotation = Quaternion.Euler(0f, UnityEngine.Random.Range(0f, 360f), 0f);
+    }
+    //Place the baby in the area
+    private void PlaceBaby()
+    {
+        Rigidbody rigidbody = penguinBaby.GetComponent<Rigidbody>();
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+        penguinBaby.transform.position = ChooseRandomPosition(transform.position, -45f, 45f, 0f, 1f) + Vector3.up * .5f;
+        penguinBaby.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+    }
+    //Spawn some number of fish in the area and set their swim speed
+    private void SpawnFish(int count, float fishSpeed)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            //spawn and place the fish
+            GameObject fishObject = Instantiate<GameObject>(fishPrefab.gameObject);
+            fishObject.transform.position = ChooseRandomPosition(transform.position, 100f, 260f, 2f, 13f) + Vector3.up * .5f;
+            fishObject.transform.rotation = Quaternion.Euler(0f, UnityEngine.Random.Range(0f, 360f), 0f);
+
+            //set the fish's parent to this area's transform
+            fishObject.transform.SetParent(transform);
+
+            //Keep track of the fish
+            fishList.Add(fishObject);
+
+            //set the fish speed
+            fishObject.GetComponent<Fish>().fishSpeed = fishSpeed;
+        }
+    }
+    //Called when the game starts
+    private void Start()
+    {
+        //On the first Heuristic Training, commenting this out so it doesn't destroy the pre-built model
+        ResetArea();
+    }
+
+    private void Update()
+    {
+        cumulativeRewardText.text = penguinAgent.GetCumulativeReward().ToString("0.00");
+    }
+}
